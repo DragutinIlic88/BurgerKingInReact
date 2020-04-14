b46a7401ffe34571a403f1eceaa542f7c1e7d234
unknown <dragutinilic88@gmail.com>
2020-04-14 11:55:52 +0200

    Added Auxiliary functional component

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,8 +1,8 @@
 import React from "react";
-import Aux from "../../hoc/Aux";
+import Aux from "../../hoc/Auxiliary";
 import classes from "./Layout.module.css";
 
-const layout = props => (
+const layout = (props) => (
   <Aux>
     <div>Toolbar, SideDrawer, Backdrop</div>
     <main className={classes.Content}>{props.children}</main>
