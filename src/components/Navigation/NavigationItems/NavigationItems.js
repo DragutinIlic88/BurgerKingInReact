fd86928a0a854cac721985fdada8db3e5af5bc80
unknown <dragutinilic88@gmail.com>
2020-04-14 14:01:18 +0200

    Navigation items component added inside Toolbar component

---
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -0,0 +1,17 @@
+import React from "react";
+import classes from "./Navigationitems.module.css";
+import NavigationItem from "./NavigationItem/NavigationItem";
+
+const navigationItems = () => (
+  <ul className={classes.NavigationItems}>
+    {/* boolean props can be passed just by name - no need to assaign them any value */}
+    <NavigationItem link="/" active>
+      Burger Builder
+    </NavigationItem>
+    <NavigationItem link="/" active>
+      Checkout
+    </NavigationItem>
+  </ul>
+);
+
+export default navigationItems;
