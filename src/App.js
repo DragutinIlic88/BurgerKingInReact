4c7fb68d5c619dab3bb5c3b446710087fea4c228
unknown <dragutinilic88@gmail.com>
2020-04-25 11:49:31 +0200

    Added Order and Checkout components and Routing system

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from "react";
+import { Route, Switch } from "react-router-dom";
 import Layout from "./hoc/Layout/Layout";
 import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
+import Checkout from "./containers/Checkout/Checkout";
 
 function App() {
   return (
     <div>
       <Layout>
-        <BurgerBuilder />
+        {/* With Switch component only first Route with  path which is matched will be  displayed. Need to be careful about the order of Route component */}
+        <Switch>
+          <Route path="/checkout" component={Checkout} />
+          {/*exact property tells that path must be the same (not prefix) */}
+          <Route path="/" exact component={BurgerBuilder} />
+        </Switch>
       </Layout>
     </div>
   );
